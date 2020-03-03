370c220597dd39c77e26158b4666d8af8c76b084
saarikoski-jules <jsaariko@student.codam.nl>
2020-03-03 14:46:15 +0100

    working version

--- print_result.c
+++ print_result.c
@@ -6,7 +6,7 @@
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/11 12:05:50 by jsaariko       #+#    #+#                */
-/*   Updated: 2020/02/25 19:28:51 by jsaariko      ########   odam.nl         */
+/*   Updated: 2020/03/03 14:38:36 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,122 +20,122 @@ int print_item(const char *str, int len)
 	return (write(1, str, len));
 }
 
-// int manage_print(const char *str, t_printf_arg **head)
-// {
-// 	t_printf_arg	*cur_arg;
-// 	char			*arg_str;
-// 	// char			*item;
-// 	int				len;
-// 	int				i;
-// 	int				ret;
-
-// 	i = 0;
-// 	len = 0;
-// 	ret = 0;
-// 	cur_arg = *head;
-// 	while (1)
-// 	{
-// 		printf("str[%d] == '%c'\n", i, str[i]);
-// 		if (str[i] == '%' || str[i] == '\0')
-// 		{
-// 			ret += print_item(str + len, i - len);
-// 			len = i + 2 + ft_strchrset(str + i + 1, "cspdiuxX%"); //plus the conversion etc
-// 			// printf("%d\n", len);
-// 			if (str[i] == '%')
-// 			{			
-// 				// printf("str[i] = '%c, i = %d'\n", str[i], i);
-// 				arg_str = execute_arg(&cur_arg);//
-// 				if (!arg_str)
-// 				{
-// 					return (-1);
-// 				}
-// 				ret += print_item(arg_str, cur_arg->field_width);
-// 				if (cur_arg->next != NULL)
-// 					cur_arg = cur_arg->next;
-// 				if (ret == -1)
-// 				{
-// 					free(arg_str);
-// 					return (-1);
-// 				}
-// 				free(arg_str);
-// 			}
-// 			if (str[i] == '\0')
-// 				return (ret);
-// 			if (i + 1 != '\0') //double check the necessity/logic here
-// 				i++;
-// 		}
-// 		i++;
-// 	}
-// 	return (ret);
-// }
-
-int write_string(const char *str, int *ret)
-{
-	int len;
-
-	len = 0;
-	// printf("\n\nWRITE STRING: '%s', RET: %d\n\n", str, *ret);
-	while(str[len] != '\0' && str[len] != '%')
-	{
-		// printf("str[%d] = %c\n", len, str[len]);
-		len++;
-	}
-	*ret += print_item(str, len);
-	// while (str[len] != '\0' || str[len] != '%')
-	// {
-	// 	printf("str[%d] = %c\n", len, str[len]);
-	// 	len++;
-	// }
-	// *ret += print_item(str, len);
-	printf("\n\nlen = %d\n\n", len);
-	return (len);
-}
-
-int write_conv(t_printf_arg *arg, int *ret)
-{
-	char *arg_str;
-	int		len;
-
-	arg_str = execute_arg(&arg);
-	// printf("\n%s\n", arg_str);
-	*ret = print_item(arg_str, arg->field_width);
-	return (1);
-}
-
 int manage_print(const char *str, t_printf_arg **head)
 {
 	t_printf_arg	*cur_arg;
-	// char			*arg_str;
-	int				ret;
+	char			*arg_str;
+	// char			*item;
+	int				len;
 	int				i;
-	int				search;
+	int				ret;
 
+	i = 0;
+	len = 0;
 	ret = 0;
 	cur_arg = *head;
-	i = 0;
-	// printf("in manage print\n");
-	while(str[i] != '\0')
+	while (1)
 	{
-		// printf("\n\n\n--------STR[%d] = %c-------------\n\n\n", i, str[i]);
-		i = i + write_string(str + i, &ret);
-		// printf("\n\n-------- i = %d -------\n\n", i);
-		if (cur_arg != NULL)
+		// printf("str[%d] == '%c'\n", i, str[i]);
+		if (str[i] == '%' || str[i] == '\0')
 		{
-			write_conv(cur_arg, &ret);
-			cur_arg = cur_arg->next;
+			ret += print_item(str + len, i - len);
+			len = i + 2 + ft_strchrset(str + i + 1, "cspdiuxX%"); //plus the conversion etc
+			// printf("%d\n", len);
+			if (str[i] == '%')
+			{			
+				// printf("str[i] = '%c, i = %d'\n", str[i], i);
+				arg_str = execute_arg(&cur_arg);//
+				if (!arg_str)
+				{
+					return (-1);
+				}
+				ret += print_item(arg_str, cur_arg->field_width);
+				if (cur_arg->next != NULL)
+					cur_arg = cur_arg->next;
+				if (ret == -1)
+				{
+					free(arg_str);
+					return (-1);
+				}
+				free(arg_str);
+			}
+			if (str[i] == '\0')
+				return (ret);
+			if (i + 1 != '\0') //double check the necessity/logic here
+				i++;
 		}
-		search = ft_strchrset(str + i + 1, "cspdiuxX%");
-		// printf("string: %s\n", str + i + 1);
-		if (search != -1)
-			i = i + search + 1;
-		// printf("'%c', %d, %d\n", str[i], i, search);
-		// i++;
-
+		i++;
 	}
-	// while (str[i] != '\0')
-	// {
-		// printf("%d\n", i);
-		// write_arg(cur_arg, &ret);
-	// }
 	return (ret);
 }
+
+// int write_string(const char *str, int *ret)
+// {
+// 	int len;
+
+// 	len = 0;
+// 	// printf("\n\nWRITE STRING: '%s', RET: %d\n\n", str, *ret);
+// 	while(str[len] != '\0' && str[len] != '%')
+// 	{
+// 		// printf("str[%d] = %c\n", len, str[len]);
+// 		len++;
+// 	}
+// 	*ret += print_item(str, len);
+// 	// while (str[len] != '\0' || str[len] != '%')
+// 	// {
+// 	// 	printf("str[%d] = %c\n", len, str[len]);
+// 	// 	len++;
+// 	// }
+// 	// *ret += print_item(str, len);
+// 	printf("\n\nlen = %d\n\n", len);
+// 	return (len);
+// }
+
+// int write_conv(t_printf_arg *arg, int *ret)
+// {
+// 	char *arg_str;
+// 	int		len;
+
+// 	arg_str = execute_arg(&arg);
+// 	// printf("\n%s\n", arg_str);
+// 	*ret = print_item(arg_str, arg->field_width);
+// 	return (1);
+// }
+
+// int manage_print(const char *str, t_printf_arg **head)
+// {
+// 	t_printf_arg	*cur_arg;
+// 	// char			*arg_str;
+// 	int				ret;
+// 	int				i;
+// 	int				search;
+
+// 	ret = 0;
+// 	cur_arg = *head;
+// 	i = 0;
+// 	// printf("in manage print\n");
+// 	while(str[i] != '\0')
+// 	{
+// 		// printf("\n\n\n--------STR[%d] = %c-------------\n\n\n", i, str[i]);
+// 		i = i + write_string(str + i, &ret);
+// 		// printf("\n\n-------- i = %d -------\n\n", i);
+// 		if (cur_arg != NULL)
+// 		{
+// 			write_conv(cur_arg, &ret);
+// 			cur_arg = cur_arg->next;
+// 		}
+// 		search = ft_strchrset(str + i + 1, "cspdiuxX%");
+// 		// printf("string: %s\n", str + i + 1);
+// 		if (search != -1)
+// 			i = i + search + 1;
+// 		// printf("'%c', %d, %d\n", str[i], i, search);
+// 		// i++;
+
+// 	}
+// 	// while (str[i] != '\0')
+// 	// {
+// 		// printf("%d\n", i);
+// 		// write_arg(cur_arg, &ret);
+// 	// }
+// 	return (ret);
+// }
